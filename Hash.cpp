4a4452a8caefa2306b7bc43a50d869c1d88f7683
sforn-null <1641832926@qq.com>
2021-06-10 21:53:53 +0800

    Hash的一般使用

---
+++ Hash.cpp
@@ -0,0 +1,48 @@
+//#include<iostream>
+//#include<unordered_set>
+//#include<unordered_map>
+//#include<map>
+//#include<set>
+//using namespace std;
+//
+//void test_unordered_map_set()
+//{
+//	//unordered  无序
+//	//unordered_set<int> us;
+//	//us.insert(4);
+//	//us.insert(2);
+//	//us.insert(1);
+//	//us.insert(5);
+//	//us.insert(6);
+//
+//	////可以去重，但不能排序
+//	//unordered_set<int>::iterator it = us.begin();
+//	//while (it != us.end())
+//	//{
+//	//	cout << *it << " ";
+//	//	++it;
+//	//}
+//	//cout << endl;
+//
+//	set<int> s;
+//	s.insert(4);
+//	s.insert(2);
+//	s.insert(1);
+//	s.insert(5);
+//	s.insert(6);
+//
+//	//既去重，也可以排序
+//	set<int>::iterator it = s.begin();
+//	while (it != s.end())
+//	{
+//		cout << *it << " ";
+//		++it;
+//	}
+//	cout << endl;
+//}
+//
+//int main()
+//{
+//	test_unordered_map_set();
+//	return 0;
+//}
